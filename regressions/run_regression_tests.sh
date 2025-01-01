c26f93452db0e13edfcbde49982eeb1d8b1c7648
varzenbot <ionlaurentiu.petre@gmail.com>
2025-01-01 03:33:07 +0100

    Fix execution command for regression test scripts in run_regression_tests.sh

--- regressions/run_regression_tests.sh
+++ regressions/run_regression_tests.sh
@@ -6,7 +6,7 @@ for TEST in ${TESTS}
 do
   (if [[ -f "regression_test${TEST}.sh" ]]
   then
-    if ! "regression_test${TEST}.sh"
+    if ! ./"regression_test${TEST}.sh"
     then
       echo "FAILED regression_test${TEST}"
       exit 1

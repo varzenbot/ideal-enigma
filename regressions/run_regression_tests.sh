f0c092a4e7270cbc4cbfd695d0003f43df744ba5
varzenbot <ionlaurentiu.petre@gmail.com>
2025-01-01 03:15:16 +0100

    Rename regression test script and add new script to run tests

---
+++ regressions/run_regression_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TESTS="1"
+
+for TEST in ${TESTS}
+do
+  (if [[ -f "regression_test${TEST}.sh" ]]
+  then
+    if ! ./regression_test${TEST}.sh
+    then
+      echo "FAILED regression_test${TEST}"
+      exit 1
+    fi
+  fi)
+done

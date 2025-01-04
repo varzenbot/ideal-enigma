6491c2e34619e2ee1a7eaec892d2935ff3d25552
varzenbot <ionlaurentiu.petre@gmail.com>
2025-01-04 19:25:35 +0100

    Update main.go to format output with windowed segments and adjust regression test expectations

--- main.go
+++ main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	output := "Hello, World!"
+
+	window_min := 0
+	window_size := 2
+	output_size := len(output)
+	for window_min+window_size <= output_size {
+		window := output[window_min : window_min+window_size]
+		fmt.Print(window + "_")
+		window_min += window_size
+	}
+	if window_min < output_size {
+		window := output[window_min:]
+		fmt.Println(window)
+	} else {
+		fmt.Println()
+	}
+	fmt.Println(output)
 }

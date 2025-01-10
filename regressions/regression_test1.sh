ddbf731f2294e1fe4befafbcf8ea1e2ab997ab6a
varzenbot <ionlaurentiu.petre@gmail.com>
2025-01-10 21:16:15 +0100

    Enhance regression test scripts with output messages for test results

--- regressions/regression_test1.sh
+++ regressions/regression_test1.sh
@@ -3,6 +3,7 @@
 TEST_OUTPUT=$(curl -s -X GET localhost:27016)
 if [[ ${TEST_OUTPUT} = "Hello, World!" ]]
 then
+  echo "PASSED $(basename $0)!"
   exit 0
 fi
 echo "ERROR: we got ->  \"${TEST_OUTPUT}\""
--- regressions/run_regression_tests.sh
+++ regressions/run_regression_tests.sh
@@ -7,15 +7,19 @@ go run ../main.go &
 
 for TEST in ${TESTS}
 do
+  echo "Running regression_test${TEST}"
   (if [[ -f "regression_test${TEST}.sh" ]]
   then
-    if ! ./"regression_test${TEST}.sh"
+    if ! ./regression_test${TEST}.sh
     then
       echo "FAILED regression_test${TEST}"
       exit 1
     fi
+    echo "PASSED regression_test${TEST}"
   fi)
 done
 
 # stop the server
 kill %1
+
+exit 0

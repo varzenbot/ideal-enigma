07b0f0dc1a7e56fa572c9c9b7b90b141a77290dd
varzenbot <ionlaurentiu.petre@gmail.com>
2025-01-10 21:23:11 +0100

    Improve regression test script to track and report failed tests

--- regressions/run_regression_tests.sh
+++ regressions/run_regression_tests.sh
@@ -5,19 +5,22 @@ TESTS="1"
 # start the server
 go run ../main.go &
 
+typeset FAILED="0"
+
 for TEST in ${TESTS}
 do
   echo "Running regression_test${TEST}"
-  (if [[ -f "regression_test${TEST}.sh" ]]
+  if [[ -f "regression_test${TEST}.sh" ]]
   then
     if ! "./regression_test${TEST}.sh"
     then
       echo "FAILED regression_test${TEST}"
+      FAILED=${TEST}
       exit 1
     fi
     echo "PASSED regression_test${TEST}"
-  fi)
-  if $? -ne 0
+  fi
+  if [[ ${FAILED} != "0" ]]
   then
     echo "ERROR: regression_test${TEST} failed"
     exit 1

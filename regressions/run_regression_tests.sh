c7cce8f68c5f6546a50799299f59fd1e5b5f9265
varzenbot <ionlaurentiu.petre@gmail.com>
2025-01-10 21:25:52 +0100

    kill the server before quiting the tests

--- regressions/run_regression_tests.sh
+++ regressions/run_regression_tests.sh
@@ -5,8 +5,6 @@ TESTS="1"
 # start the server
 go run ../main.go &
 
-typeset FAILED="0"
-
 for TEST in ${TESTS}
 do
   echo "Running regression_test${TEST}"
@@ -15,15 +13,10 @@ do
     if ! "./regression_test${TEST}.sh"
     then
       echo "FAILED regression_test${TEST}"
-      FAILED=${TEST}
+      kill %1
       exit 1
     fi
     echo "PASSED regression_test${TEST}"
-  fi
-  if [[ ${FAILED} != "0" ]]
-  then
-    echo "ERROR: regression_test${TEST} failed"
-    exit 1
   fi
 done
 

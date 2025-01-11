a48cc95e19c6f3e6bccf4d2a080577a312d9ff77
varzenbot <ionlaurentiu.petre@gmail.com>
2025-01-11 16:01:53 +0100

    Add /windows endpoint and regression tests for dynamic greeting

--- main.go
+++ main.go
@@ -2,9 +2,25 @@ package main
 
 import "net/http"
 
+func handle_windows(w http.ResponseWriter, r *http.Request) {
+	// read arguments from the path and build the response
+
+	// read arg
+
+	// build response
+	output := "Hello, "
+	for _, entry := range r.URL.Query() {
+		output += entry[0] + ", "
+	}
+	output += "!"
+	w.Write([]byte(output))
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})
+
+	http.HandleFunc("/windows", handle_windows)
 	http.ListenAndServe(":27016", nil)
 }
